b8e29b25c01273b185c404d0757a48a61ca642c0
Liam Simpkin <lsimpkin@e1z2r3p26.42.us.org>
2019-04-06 22:34:34 -0700

    added struct

--- ft_printf.h
+++ ft_printf.h
@@ -3,7 +3,29 @@
 
 #include "libft/libft.h"
 #include <stdarg.h> // man 3 stdarg
+#define SPECI "scd"
 
 void ft_printf(const char *format, ...);
+
+typedef struct s_table
+{
+	char *conversions;
+	char specifier;
+	char *flag;
+	int error;
+}                conversion_table;
+
+int search_specifiers(const char *format);
+int search_flags(const char *format);
+int search_length(const char *format);
+
+/*
+min number of chars to be printed. If the value to be printed is
+shorter than this number, the reult is padded with blank spaces
+*/
+int search_width(const char *format);
+int search_precision(const char *format);
+char *ft_search(const char *str, char*search);
+
 
 #endif
